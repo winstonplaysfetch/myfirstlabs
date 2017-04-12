f07194e23c360256dd1f77f6630166b1af0ef403
jdaly8 <jennyndaly@gmail.com>
2017-04-12 05:02:19 +0000

    Added a link to edit in Show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,11 +6,28 @@ class ProjectsController < ApplicationController
   def new
   end
   
+  def show
+    @project = Project.find(params[:id])
+  end
+  
   def create
     @project = Project.new(project_params)
     
     @project.save
     redirect_to @project
+  end
+  
+  def edit
+    @project = Project.find(params[:id])
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
   end
   
   def destroy
@@ -19,11 +36,6 @@ class ProjectsController < ApplicationController
     
     redirect_to projects_path
   end
-  
-  def show
-    @project = Project.find(params[:id])
-  end
-
 
 private
   def project_params
