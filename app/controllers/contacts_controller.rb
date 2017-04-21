b04b1d24c16bd3e7738492555e1fa231b7b62cea
jdaly8 <jennyndaly@gmail.com>
2017-04-21 03:05:53 +0000

    linked contacts index fields to actual field names, created a show.html to reroute to after creating, deleted contact.html.erb in projects folder

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,12 +9,15 @@ class ContactsController < ApplicationController
     
     def create
         @contact = Contact.new(contact_params)
-        
         if @contact.save
             redirect_to @contact
         else
             render 'new'
         end
+    end
+    
+    def show
+        @contact = Contact.find(params[:id])
     end
     
     private
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,3 @@
 class Contact < ApplicationRecord
-  belongs_to :project
+  #belongs_to :project
 end
