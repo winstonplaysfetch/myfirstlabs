05ebbae7e916fb255e5c3dde237ca9a635c19aa2
Vincent Popp <vpopp@uncc.edu>
2017-04-27 20:17:47 +0000

    fixed ambiguous cucmber test error

--- features/step_definitions/contact_steps.rb
+++ features/step_definitions/contact_steps.rb
@@ -1,8 +1,8 @@
-Given(/^I am on the home page$/) do
+Given(/^I am on the home page of the website$/) do
   visit root_path
 end
 
-When(/^I click on the "([^"]*)" link$/) do |link_name|
+When(/^I click on the "([^"]*)" link button$/) do |link_name|
   click_link link_name
 end
 
