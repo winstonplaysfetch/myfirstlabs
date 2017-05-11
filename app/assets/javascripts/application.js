7cca01f24dd92b26299cb147e224d0441b64b4f6
jdaly8 <jennyndaly@gmail.com>
2017-05-11 16:24:19 +0000

    added link to Tindie store on projects/index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var index = 1;
+
+function plusIndex(n){
+	index = index +1;
+	showImage(index);
+}
+
+function showImage(n){
+	var i;
+	var x = document.getElementsByClassName("slides");
+	if(n > x.length){index = 1};
+	if(n < 1){index = x.length};
+	for(i=0;i<x.length;i++){
+		x[i].style.display = "none";	
+	}
+	x[index-1].style.display = "block";
+}
