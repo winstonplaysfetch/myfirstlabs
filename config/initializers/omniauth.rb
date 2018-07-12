27741f2fca0b4b510f8414d962ead228e91190a9
jdaly8 <jennyndaly@gmail.com>
2018-07-12 06:08:32 +0000

    removed secret containing files from repo

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '581425948407-ngp00hmcv116n9ce129g8tii5d8dlm0h.apps.googleusercontent.com', 'uEqJba7I-TVVtEKZE8oITVV0', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
