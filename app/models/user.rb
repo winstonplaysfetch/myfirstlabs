0717ac377ffc64919faedd4989ae5b6cc76465cd
jdaly8 <jennyndaly@gmail.com>
2018-07-20 19:12:20 +0000

    re-did Sessions controller, Users controller, redefined routes for sessions, wrote a test, and created a login view. Login feature works at this point

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 # Schema: User(name:string, password_digest:string)
 class User < ActiveRecord::Base
   has_secure_password
+  
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
